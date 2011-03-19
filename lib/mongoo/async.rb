aa96fdcbcb3adfb9f0ca8fb3339d8e902831b126
Ben Myles <ben.myles@gmail.com>
2011-03-19 16:20:38 -0700

    forget logging with rails

--- lib/mongoo/async.rb
+++ lib/mongoo/async.rb
@@ -1,4 +1,4 @@
-if ENV["ASYNC"] == "1" || (ENV["SYNC"] != "1" && (defined?(EM) && EM.reactor_running?))
+if ENV["MONGOO_ASYNC"] == "1" || (ENV["MONGOO_SYNC"] != "1" && (defined?(EM) && EM.reactor_running?))
   require "em-synchrony"
   require "em-synchrony/tcpsocket"
   module Mongo
@@ -29,11 +29,7 @@ if ENV["ASYNC"] == "1" || (ENV["SYNC"] != "1" && (defined?(EM) && EM.reactor_run
     end
   end
 
-  if defined?(::Rails)
-    ::Rails.logger.info "* Mongoo Running in Asynchronous Mode"
-  else
-    puts "* Mongoo Running in Asynchronous Mode" if ENV["DEBUG"] == "1"
-  end
+  puts "* Mongoo Running in Asynchronous Mode" if ENV["MONGOO_DEBUG"] == "1"
 else
   module Mongoo
     def self.mode
@@ -41,9 +37,5 @@ else
     end
   end
   
-  if defined?(::Rails)
-    ::Rails.logger.info "* Mongoo Running in Synchronous Mode"
-  else
-    puts "* Mongoo Running in Synchronous Mode" if ENV["DEBUG"] == "1"
-  end
+  puts "* Mongoo Running in Synchronous Mode" if ENV["MONGOO_DEBUG"] == "1"
 end
