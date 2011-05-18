1994a61b26521343a013d8d3b24efb0a92aeca1f
Ben Myles <ben.myles@gmail.com>
2011-05-17 20:46:47 -0700

    really should be doing deep_stringify_keys

--- lib/mongoo/hash_ext.rb
+++ lib/mongoo/hash_ext.rb
@@ -8,7 +8,7 @@ module Mongoo
       keys.each do |key|
         self[key.to_s] = delete(key)
         if self[key.to_s].is_a?(Hash)
-          self[key.to_s].stringify_keys!
+          self[key.to_s].deep_stringify_keys!
         end
       end
       self
