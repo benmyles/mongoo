8934dc4f96826ecd82bec9d2bbe3f151ade7f84a
Ben Myles <ben.myles@gmail.com>
2011-05-29 18:35:36 -0700

    There are some problems with connection failover for the async pool, so temporarily disable pooling.

--- lib/mongoo/async.rb
+++ lib/mongoo/async.rb
@@ -34,55 +34,56 @@ module Mongo
     #
     def initialize(connection, host, port, opts={})
       @connection  = connection
-
       @host, @port = host, port
 
-      # Pool size and timeout.
-      @size      = opts[:size] || 1
-      @timeout   = opts[:timeout]   || 5.0
+      # # Pool size and timeout.
+      # @size      = opts[:size]    || 1
+      # @timeout   = opts[:timeout] || 5.0
 
-      # Operations to perform on a socket
-      @socket_ops = Hash.new { |h, k| h[k] = [] }
+      # # Operations to perform on a socket
+      # @socket_ops = Hash.new { |h, k| h[k] = [] }
 
-      @all       = []
-      @reserved  = {}   # map of in-progress connections
-      @available = []   # pool of free connections
-      @pending   = []   # pending reservations (FIFO)
+      # @all       = []
+      # @reserved  = {}   # map of in-progress connections
+      # @available = []   # pool of free connections
+      # @pending   = []   # pending reservations (FIFO)
 
-      setup_pool!(host, port)
+      # setup_pool!(host, port)
     end
 
     def setup_pool!(host, port)
-      @size.times do |i|
-        sock = checkout_new_socket(host, port)
-        @all << sock
-        @available << sock
-      end
+      true
+      # @size.times do |i|
+      #   sock = checkout_new_socket(host, port)
+      #   @all << sock
+      #   @available << sock
+      # end
     end
 
     def close
-      @all.each do |sock|
-        begin
-          sock.close
-        rescue IOError => ex
-          warn "IOError when attempting to close socket connected to #{@host}:#{@port}: #{ex.inspect}"
-        end
-      end
-      @host = @port = nil
-      @all.clear
-      @reserved.clear
-      @available.clear
-      @pending.clear
+      true
+      # @all.each do |sock|
+      #   begin
+      #     sock.close
+      #   rescue IOError => ex
+      #     warn "IOError when attempting to close socket connected to #{@host}:#{@port}: #{ex.inspect}"
+      #   end
+      # end
+      # @host = @port = nil
+      # @all.clear
+      # @reserved.clear
+      # @available.clear
+      # @pending.clear
     end
 
     # Return a socket to the pool.
     def checkin(socket)
-      fiber = Fiber.current
-
-      @available.push(@reserved.delete(fiber.object_id))
-      if pending = @pending.shift
-        pending.resume
-      end
+      socket.close
+      # fiber = Fiber.current
+      # @available.push(@reserved.delete(fiber.object_id))
+      # if pending = @pending.shift
+      #   pending.resume
+      # end
       true
     end
 
@@ -91,36 +92,39 @@ module Mongo
     # So we store the apply_authentication method, and this will be
     # applied right before the next use of each socket.
     def authenticate_existing
-      @all.each do |socket|
-        @socket_ops[socket] << Proc.new do
-          @connection.apply_saved_authentication(:socket => socket)
-        end
-      end
+      true
+      # @all.each do |socket|
+      #   @socket_ops[socket] << Proc.new do
+      #     @connection.apply_saved_authentication(:socket => socket)
+      #   end
+      # end
     end
 
     # Store the logout op for each existing socket to be applied before
     # the next use of each socket.
     def logout_existing(db)
-      @all.each do |socket|
-        @socket_ops[socket] << Proc.new do
-          @connection.db(db).issue_logout(:socket => socket)
-        end
-      end
+      true
+      # @all.each do |socket|
+      #   @socket_ops[socket] << Proc.new do
+      #     @connection.db(db).issue_logout(:socket => socket)
+      #   end
+      # end
     end
 
     # Check out an existing socket or create a new socket if the maximum
     # pool size has not been exceeded. Otherwise, wait for the next
     # available socket.
     def checkout
-      fiber = Fiber.current
-      #puts "[P: #{@pending.size}, A: #{@available.size}, ALL: #{@all.size}]"
-      if socket = @available.pop
-        @reserved[fiber.object_id] = socket
-        socket
-      else
-        Fiber.yield @pending.push fiber
-        checkout
-      end
+      checkout_new_socket(@host, @port)
+      # fiber = Fiber.current
+      # #puts "[P: #{@pending.size}, A: #{@available.size}, ALL: #{@all.size}]"
+      # if socket = @available.pop
+      #   @reserved[fiber.object_id] = socket
+      #   socket
+      # else
+      #   Fiber.yield @pending.push fiber
+      #   checkout
+      # end
     end
 
     # Adds a new socket to the pool and checks it out.
@@ -128,8 +132,7 @@ module Mongo
     # This method is called exclusively from #checkout;
     # therefore, it runs within a mutex.
     def checkout_new_socket(host, port)
-      return nil if @all.size >= @size
-
+      # return nil if @all.size >= @size
       begin
         socket = TCPSocket.new(host, port)
         socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
